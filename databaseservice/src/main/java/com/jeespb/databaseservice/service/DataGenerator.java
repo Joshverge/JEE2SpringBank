1f5c6443f6801ac4a510854c260148bbf2c3f2ad
Joshua Varghese <joshdnv2@gmail.com>
2021-08-11 18:45:58 -0500

    Data generator from json file

--- databaseservice/src/main/java/com/jeespb/databaseservice/service/DataGenerator.java
+++ databaseservice/src/main/java/com/jeespb/databaseservice/service/DataGenerator.java
@@ -1,43 +1,57 @@
 package com.jeespb.databaseservice.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jeespb.databaseservice.model.User;
 import com.jeespb.databaseservice.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
 @Component
+@ConditionalOnProperty(
+        value = "data.generator.enabled",
+        havingValue = "true")
 public class DataGenerator {
 
     private final Logger logger = LoggerFactory.getLogger(DataGenerator.class);
 
-    private final UserRepository userRepository;
+    @Value("classpath:data/user.json")
+    private Resource userResourceFile;
 
-    public DataGenerator(UserRepository userRepository) {
+    private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
+    private final ResourceLoader resourceLoader;
+
+    public DataGenerator(UserRepository userRepository, ResourceLoader resourceLoader) {
         this.userRepository = userRepository;
+        this.resourceLoader = resourceLoader;
+        this.objectMapper = new ObjectMapper();
     }
 
     @PostConstruct
-    public void init() {
+    public void init() throws IOException {
         generateData();
     }
 
-    private void generateData() {
+    private void generateData() throws IOException {
         generateUsers();
     }
 
-    private void generateUsers() {
+    private void generateUsers() throws IOException {
         logger.info("Generating users...");
-        User user1 = new User();
-        user1.setUsername("abc@xyz.com");
-        user1.setPassword("asdf123");
-        user1.setCustomerName("Name 1");
-        user1.setUserType("tier-1");
-        userRepository.save(user1);
-
-        logger.info("Generated users");
+        InputStream inputStream = userResourceFile.getInputStream();
+        User[] users = objectMapper.readValue(inputStream, User[].class);
+        Arrays.stream(users).forEach(userRepository::save);
+        logger.info("Generated {} users", users.length);
     }
 
 }
