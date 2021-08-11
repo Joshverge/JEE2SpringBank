3284851f73935f177b105e5e4ba55ef01d2a5083
Joshua Varghese <joshdnv2@gmail.com>
2021-08-11 18:50:37 -0500

    Completed endpoints for database service

--- databaseservice/src/main/java/com/jeespb/databaseservice/repository/RewardRepository.java
+++ databaseservice/src/main/java/com/jeespb/databaseservice/repository/RewardRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RewardRepository extends CrudRepository<Reward, Integer>, JpaSpecificationExecutor<Reward> {
+
+    List<Reward> findByUsername(String username);
 
 }
--- databaseservice/src/main/java/com/jeespb/databaseservice/repository/TransactionRepository.java
+++ databaseservice/src/main/java/com/jeespb/databaseservice/repository/TransactionRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends CrudRepository<Transaction, Integer>, JpaSpecificationExecutor<Transaction> {
+
+    List<Transaction> findByAccountNumber(String accountNumber);
 
 }
